5ced976cc4cd60941d166801064bcba3cc626d39
Jennifer López <jen1992lo@gmail.com>
2024-02-15 09:22:46 +0100

    Ejercicio 13 terminado

--- Cliente/Programacion/js/js-13/index.js
+++ Cliente/Programacion/js/js-13/index.js
@@ -1,8 +1,19 @@
 function parOImpar(num){
-    return num % 2 == 0 ? "Par" : "Impar";
+    if(typeof num !== 'number'){
+        return "Ingrese un numero valido"
+    }
+    
+   return num % 2 == 0 ? "El numero es par" : "El numero es impar";
 }
 
 function saberPar(){
     let numero = Number(document.getElementById('numero').value)
-    document.getElementById("resultado").innerHTML = "Tu numero es: " + parOImpar(numero);
+    
+    if(isNaN(numero)){
+        alert("Ingrese un numero valido")
+    }else{
+
+        document.getElementById("resultado").innerHTML =  parOImpar(numero);
+    }
+
 }
