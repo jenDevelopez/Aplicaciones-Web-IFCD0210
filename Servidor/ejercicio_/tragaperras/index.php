02af16ee52e97c103633f528e98324ecadb9aec5
Jennifer <jen1992lo@gmail.com>
2024-04-25 13:58:54 +0200

    clonando repo de nuevo

---
+++ Servidor/ejercicio_/tragaperras/index.php
@@ -0,0 +1,29 @@
+<?php 
+$numFrutas = 8;
+function generarFrutaAleatoria($numMax) {
+  $numeroAleatorio = rand(0,$numMax);
+}
+
+?>
+<!DOCTYPE html>
+<html lang="es">
+<head>
+  <meta charset="utf-8">
+  <title>
+    Tragaperras (1).
+    Minijuegos.
+    Escriba aquí su nombre
+  </title>
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <link rel="stylesheet" href="style.css">
+</head>
+
+<body>
+  <h1>Tragaperras</h1>
+
+
+  <footer>
+    <p>Jennifer Lopez Montero</p>
+  </footer>
+</body>
+</html>
