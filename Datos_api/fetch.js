254ab5c9128e3074290f2a19f9589d94194f5c7e
Jennifer <jen1992lo@gmail.com>
2024-05-10 11:31:08 +0200

    comentando el codigo

--- Datos_api/fetch.js
+++ Datos_api/fetch.js
@@ -1,17 +1,25 @@
-async function traerDatos(){
-  const res  = await fetch('https://jsonplaceholder.typicode.com/todos?limit=50')
+// Función asincrónica que hace una petición a una API
+async function traerDatos() {
+  // Hace la petición a la API
+  const res = await fetch('https://jsonplaceholder.typicode.com/todos?limit=50')
+  // Convierte la respuesta a formato JSON
   const data = await res.json()
-
+  // Devuelve los datos obtenidos
   return data
 }
 
-document.addEventListener('DOMContentLoaded',() => {
+// Añade un evento al DOMContentLoaded
+document.addEventListener('DOMContentLoaded', () => {
+  // Llama a la función traerDatos y espera a que se resuelva la promesa
   traerDatos().then((data) => {
-    const li = document.createElement
-    data.map((dato) => {
-      document.getElementById('lista').innerHTML += `<li>${dato.title}</li>`      
-    } )
+    // Crea un elemento li
+    const li = document.createElement('li')
+    // Recorre cada elemento de data
+    data.forEach((dato) => {
+      // Establece el texto del elemento li con el título del dato
+      li.textContent = dato.title
+      // Añade el elemento li al elemento con id 'lista'
+      document.getElementById('lista').appendChild(li)
+    })
   })
 })
-
-
